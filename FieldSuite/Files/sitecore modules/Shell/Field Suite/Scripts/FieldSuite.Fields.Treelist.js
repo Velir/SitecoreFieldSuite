2429f262c6d9a90ba89302bb24ff7eaa5026b3a1
Adam Lamarre <adam.lamarre@velir.com>
2012-12-13 12:21:43 -0500

    added event object to treelist AddItem call.

--- FieldSuite/Files/sitecore modules/Shell/Field Suite/Scripts/FieldSuite.Fields.Treelist.js
+++ FieldSuite/Files/sitecore modules/Shell/Field Suite/Scripts/FieldSuite.Fields.Treelist.js
@@ -21,13 +21,20 @@ if (FieldSuite.Html == null) {
 	FieldSuite.Html = [];
 }
 
-FieldSuite.Fields.Treelist.SelectedItem = function (fieldId) {
-	var selectedItems = $$('#' + fieldId + ' .scContentTreeNodeActive');
-	if (selectedItems == null || selectedItems.length == 0) {
-		return null;
-	}
+FieldSuite.Fields.Treelist.SelectedItem = function (fieldId, event) {
+    if (event) {
+        var selectedItem = Event.element(event);
+        if (selectedItem) {
+            return selectedItem;
+        }
+    }
 
-	return selectedItems[0];
+    var selectedItems = $('#' + fieldId + ' .scContentTreeNodeActive');
+    if (selectedItems == null || selectedItems.length == 0) {
+        return null;
+    }
+
+    return selectedItems[0];
 }
 
 FieldSuite.Fields.Treelist.AddItemToContent = function (fieldId, html) {
@@ -46,10 +53,10 @@ FieldSuite.Fields.Treelist.AddItemToContent = function (fieldId, html) {
 	return false;
 }
 
-FieldSuite.Fields.Treelist.AddItem = function (fieldId) {
-
+FieldSuite.Fields.Treelist.AddItem = function (fieldId, event) {
+	
 	//get selected item
-	var selectedItem = FieldSuite.Fields.Treelist.SelectedItem(fieldId);
+	var selectedItem = FieldSuite.Fields.Treelist.SelectedItem(fieldId, event);
 	if (selectedItem == null) {
 		alert('Please select an item');
 		return;
@@ -83,8 +90,13 @@ FieldSuite.Fields.Treelist.AddItem = function (fieldId) {
 		includedTemplates = includedElement.value;
 	}
 
+	var database = '';
+	var databaseElement = $(fieldId + '_all_Database');
+	if (databaseElement != null && includedElement.value != null) {
+		database = databaseElement.value;
+	}
 	//launch serverside command
-	scForm.invoke('fieldsuite:treelist.additem(fieldid=' + fieldId + ', itemid=' + itemId + ', excludedTemplates=' + excludedTemplates + ', includedTemplates=' + includedTemplates + ')');
+	scForm.invoke('fieldsuite:treelist.additem(fieldid=' + fieldId + ', itemid=' + itemId + ', excludedTemplates=' + excludedTemplates + ', includedTemplates=' + includedTemplates + ', database=' + database + ')');
 	return false;
 }
 
